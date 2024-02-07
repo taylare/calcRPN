ea061aa26500d8350ad03351774b8efc18522ed3
Tayla Rechichi <tay.rechichi32@gmail.com>
2024-02-06 20:51:36 -0800

    Created CalcBrain methods and completed source code.

--- src/calcrpn/CalcBrain.java
+++ src/calcrpn/CalcBrain.java
@@ -19,31 +19,72 @@ public class CalcBrain implements CalcRPNOperations {
     
     @Override
     public String digit(String digit) {
-       
+       operand += digit;
+       return digit;
     }
 
     @Override
     public String operator(String op) {
+        if (!operand.isEmpty()) {
+         return operand;
+        }
+        if (results.size() < 2){
+            return "";
+        }
         
+        float a = results.pop();
+        float b = results.pop();
+        float c = 0;
+        
+        switch (op) {
+            case "+":
+                c = a + b;
+                break;
+            case "-":
+                c = b - a;
+                break;
+            case "*":
+                c = b * a;
+                break;
+            case "/":
+                c = b / a;
+        }
+        
+        results.push(c);
+        return Float.toString(c);
+            
     }
 
     @Override
     public String clearEntry() {
-  
+        operand = "";
+        return "";
     }
 
     @Override
     public String clear() {
-      
+        results.clear();
+        operand = "";
+        return "";
     }
 
     @Override
     public String enterPressed() {
-        
+        if (!operand.isBlank()) {
+             results.push(Float.parseFloat(operand));
+             operand = "";
+             return " ";
+        }else {
+            return "";
+        }
     }
 
     @Override
     public String addDecimal() {
-       
+        if (!operand.contains(".")){
+            operand += ".";
+            return ".";
+        }
+        return "";
     }
  } 
